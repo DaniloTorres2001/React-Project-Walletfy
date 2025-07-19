c3aa027c8db473712cd241b23c60ca3b93bb6d25
Danilo Torres <datv41039@gmail.com>
2025-07-19 09:29:32 -0500

    Commit Language change

--- walletfy/src/routes/events.tsx
+++ walletfy/src/routes/events.tsx
@@ -112,7 +112,7 @@ function EventsPage() {
             </div>
 
             <p className="text-sm text-gray-500 mb-6">
-                You have {totalEventos} events in {agrupado.length} months
+                Tienes {totalEventos} eventos en {agrupado.length} meses
             </p>
 
             <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6">
